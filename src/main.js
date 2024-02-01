2fcff007bfb85221844680e00d53a9515d7c9527
Jann3 <jannecleary@hotmail.com>
2024-02-01 14:28:16 +0000

    main ready function to use promises

--- src/main.js
+++ src/main.js
@@ -295,7 +295,8 @@ ipcMain.on("error", (event, message) => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", () => {
+
+app.whenReady().then(() => {
   app.allowRendererProcessReuse = true;
   createWindow();
 });
