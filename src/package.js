47c105819e6e33980eae5d573e8519e234572f79
Jann3 <jannecleary@hotmail.com>
2024-03-02 20:50:24 +0000

    Removed Msi installer for now

--- src/package.js
+++ src/package.js
@@ -5,11 +5,10 @@ const packageJson = require("../package.json");
 let resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: "./build/win",
     outputDirectory: "./dist/win",
-    authors: "Jann3",
-    exe: "DejaViewer.exe",
-    setupExe: "DejaViewer-" + packageJson.version + ".exe",
-    setupMsi: "DejaViewer-" + packageJson.version + ".msi",
-    noMsi: false
+    authors: "jann3",
+    exe: "dejaviewer.exe",
+    setupExe: "dejaviewer-" + packageJson.version + ".exe",
+    noMsi: true
 });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
